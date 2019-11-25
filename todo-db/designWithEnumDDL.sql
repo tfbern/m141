0cd23bc00e21981a6f3d49a24f1f7baf96871351
samhess <sam.hess@me.com>
2019-11-25 11:40:41 +0100

    dix ddl

--- todo-db/designWithEnumDDL.sql
+++ todo-db/designWithEnumDDL.sql
@@ -1,24 +1,8 @@
 CREATE SCHEMA m141;
 USE m141;
 
-CREATE TABLE IF NOT EXISTS `m141`.`task` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT,
-  `text` VARCHAR(45) NOT NULL,
-  `startdate` DATE NULL DEFAULT (curdate()),
-  `duedate` DATE NULL DEFAULT NULL,
-  `priority` ENUM('niedrig', 'normal', 'hoch') NOT NULL DEFAULT 'normal',
-  `status` ENUM('nicht begonnen', 'in Bearbeitung', 'erledigt', 'wartet auf jemanden', 'zurückgestellt') NOT NULL DEFAULT 'nicht begonnen',
-  `user` INT(11) NULL DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `user_idx` (`user` ASC) VISIBLE,
-  CONSTRAINT `user`
-    FOREIGN KEY (`user`)
-    REFERENCES `m141`.`user` (`id`)
-    ON DELETE RESTRICT
-    ON UPDATE RESTRICT)
-
 CREATE TABLE IF NOT EXISTS `m141`.`user` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `id` INT NOT NULL AUTO_INCREMENT,
   `username` VARCHAR(45) NOT NULL,
   `password` VARCHAR(255) NULL DEFAULT NULL,
   `registered` DATETIME NULL DEFAULT NULL,
@@ -26,7 +10,23 @@ CREATE TABLE IF NOT EXISTS `m141`.`user` (
   `lastname` VARCHAR(45) NULL DEFAULT NULL,
   `firstname` VARCHAR(45) NULL DEFAULT NULL,
   `fullname` VARCHAR(91) GENERATED ALWAYS AS (concat(`firstname`,_utf8mb4' ',`lastname`)) VIRTUAL,
-  PRIMARY KEY (`id`))
+  PRIMARY KEY (`id`));
+  
+  CREATE TABLE IF NOT EXISTS `m141`.`task` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `text` VARCHAR(45) NOT NULL,
+  `startdate` DATE NULL DEFAULT (curdate()),
+  `duedate` DATE NULL DEFAULT NULL,
+  `priority` ENUM('niedrig', 'normal', 'hoch') NOT NULL DEFAULT 'normal',
+  `status` ENUM('nicht begonnen', 'in Bearbeitung', 'erledigt', 'wartet auf jemanden', 'zurückgestellt') NOT NULL DEFAULT 'nicht begonnen',
+  `user` INT NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `user_idx` (`user` ASC) VISIBLE,
+  CONSTRAINT `user`
+    FOREIGN KEY (`user`)
+    REFERENCES `m141`.`user` (`id`)
+    ON DELETE RESTRICT
+    ON UPDATE RESTRICT);
 
 /* Fill in some test user */
 INSERT INTO `user`(firstname,lastname,username,registered) 
